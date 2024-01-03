f2941e8a6f6442b178dc4b5c4b5ab25b8e91abe6
Pcosby5 <pcosby50@gmail.com>
2024-01-03 08:52:32 +0100

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,26 +9,19 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	/*declare name of variable new node*/
-	binary_tree_t *new_node;
+	binary_tree_t *new_node; /*declare name of variable new node*/
 
-	/*Allocate memory for the new node*/
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t)); /*Allocate memory for the new node*/
 
-	/*Check if memory allocation was successful*/
-	if (new_node == NULL)
+	if (new_node == NULL) /*Check if memory allocation was successful*/
 	{
-		/*Return null if memory allocation of new node fails*/
-		return (NULL);
+		return (NULL); /*Return null if memory allocation of new node fails*/
 	}
-	/*Set the value of the new node*/
-	new_node->n = value;
-	/*Assign the parent node for new node*/
-	new_node->parent = parent;
-	/*Initialize left and right child pointers to Null*/
-	new_node->left = NULL;
+
+	new_node->n = value; /*Set the value of the new node*/
+	new_node->parent = parent; /*Assign the parent node for new node*/
+	new_node->left = NULL; /*Initialize left and right child pointers to Null*/
 	new_node->right = NULL;
 
-	/*Return a pointer to the newly created node*/
-	return (new_node);
+	return (new_node); /*Return a pointer to the newly created node*/
 }
